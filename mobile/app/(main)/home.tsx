5e1c521c83f88d104d9e99301231a957f54d978b
Sandip-Chavda <chavdas766@gmail.com>
2025-07-31 11:43:03 +0530

    Profile Screen UI Implemented

--- mobile/app/(main)/home.tsx
+++ mobile/app/(main)/home.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React, { useEffect } from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
-import { colors } from "@/constants/theme";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
 import { useAuth } from "@/context/authContext";
 import Button from "@/components/Button";
 import { testSocket } from "@/socket/socketEvents";
+import { verticalScale } from "@/utils/styling";
+import * as Icons from "phosphor-react-native";
+import { useRouter } from "expo-router";
 
 const HomeScreen = () => {
-  const { signOut } = useAuth();
+  const { user: currentUser } = useAuth();
+
+  const router = useRouter();
 
   // useEffect(() => {
   //   testSocket(testSocketCallbackHandler);
@@ -23,23 +28,106 @@ const HomeScreen = () => {
   //   console.log("Get response from the testSocket event", data);
   // };
 
-  const handleLogout = async () => {
-    await signOut();
-  };
-
   return (
-    <ScreenWrapper>
-      <Typo size={28} color={colors.white} fontWeight={"800"}>
-        HomeScreen
-      </Typo>
+    <ScreenWrapper showPattern bgOpacity={0.4}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={{ flex: 1 }}>
+            <Typo
+              color={colors.neutral200}
+              size={19}
+              textProps={{ numberOfLines: 1 }}
+            >
+              Welcome,{" "}
+              <Typo color={colors.white} size={20} fontWeight={"800"}>
+                {currentUser?.name}{" "}
+              </Typo>
+              🙂
+            </Typo>
+          </View>
+          <TouchableOpacity
+            style={styles.settingIcon}
+            onPress={() => router.push("/(main)/profileModal")}
+          >
+            <Icons.GearSixIcon
+              color={colors.white}
+              weight="fill"
+              size={verticalScale(22)}
+            />
+          </TouchableOpacity>
+        </View>
 
-      <Button onPress={handleLogout}>
-        <Typo>Loout</Typo>
-      </Button>
+        <View style={styles.content}></View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: spacingX._20,
+    gap: spacingY._15,
+    paddingTop: spacingY._15,
+    paddingBottom: spacingY._20,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    flex: 1,
+    backgroundColor: colors.white,
+    borderTopLeftRadius: radius._50,
+    borderTopRightRadius: radius._50,
+    borderCurve: "continuous",
+    overflow: "hidden",
+    paddingHorizontal: spacingX._20,
+  },
+  navbar: {
+    flexDirection: "row",
+    gap: spacingX._15,
+    alignItems: "center",
+    paddingHorizontal: spacingX._10,
+  },
+  tabs: {
+    flexDirection: "row",
+    gap: spacingX._10,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  tabStyle: {
+    paddingVertical: spacingY._10,
+    paddingHorizontal: spacingX._20,
+    borderRadius: radius.full,
+    backgroundColor: colors.neutral100,
+  },
+  activeTabStyle: {
+    backgroundColor: colors.primaryLight,
+  },
+  conversationList: {
+    paddingVertical: spacingY._20,
+  },
+  settingIcon: {
+    padding: spacingY._10,
+    backgroundColor: colors.neutral700,
+    borderRadius: radius.full,
+  },
+  floatingButton: {
+    height: verticalScale(50),
+    width: verticalScale(50),
+    borderRadius: 100,
+    position: "absolute",
+    bottom: verticalScale(30),
+    right: verticalScale(30),
+  },
+});
---
+++ mobile/app/(main)/profileModal.tsx
@@ -0,0 +1,205 @@
+import {
+  Alert,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { scale, verticalScale } from "@/utils/styling";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Header from "@/components/Header";
+import * as Icons from "phosphor-react-native";
+import BackButton from "@/components/BackButton";
+import Avatar from "@/components/Avatar";
+import Typo from "@/components/Typo";
+import Input from "@/components/Input";
+import { useAuth } from "@/context/authContext";
+import { UserDataProps } from "@/types";
+import Button from "@/components/Button";
+import { useRouter } from "expo-router";
+
+const ProfileModal = () => {
+  const { user, signOut } = useAuth();
+  const router = useRouter();
+
+  const [userData, setUserData] = useState<UserDataProps>({
+    name: "",
+    email: "",
+    avatar: null,
+  });
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setUserData({
+      name: user?.name || "",
+      email: user?.email || "",
+      avatar: user?.avatar,
+    });
+  }, [user]);
+
+  const onSubmit = async () => {};
+
+  const handleLogout = async () => {
+    router.back();
+    await signOut();
+  };
+
+  const showLogoutAlert = () => {
+    Alert.alert("Confirm Logout", "Are you sure you want to logout?", [
+      {
+        text: "Cancel",
+        style: "cancel",
+      },
+      {
+        text: "Logout",
+        style: "destructive",
+        onPress: () => handleLogout(),
+      },
+    ]);
+  };
+
+  return (
+    <ScreenWrapper isModal>
+      <View style={styles.container}>
+        <Header
+          title="Update Profile"
+          leftIcon={
+            Platform.OS === "android" && <BackButton color={colors.black} />
+          }
+          style={{ marginVertical: spacingY._15 }}
+        />
+
+        <ScrollView contentContainerStyle={styles.form}>
+          <View style={styles.avatarContainer}>
+            <Avatar uri={null} size={170} />
+            <TouchableOpacity style={styles.edition}>
+              <Icons.PencilIcon
+                size={verticalScale(20)}
+                color={colors.neutral800}
+              />
+            </TouchableOpacity>
+          </View>
+
+          <View style={{ gap: spacingY._20 }}>
+            <View style={styles.inputContainer}>
+              <Typo style={{ paddingLeft: spacingX._10 }}>Email</Typo>
+              <Input
+                value={userData.email}
+                editable={false}
+                onChangeText={(value) =>
+                  setUserData({ ...userData, email: value })
+                }
+                containerStyle={{
+                  borderColor: colors.neutral350,
+                  paddingLeft: spacingX._20,
+                  backgroundColor: colors.neutral300,
+                }}
+              />
+            </View>
+
+            <View style={styles.inputContainer}>
+              <Typo style={{ paddingLeft: spacingX._10 }}>Name</Typo>
+              <Input
+                value={userData.name}
+                onChangeText={(value) =>
+                  setUserData({ ...userData, name: value })
+                }
+                containerStyle={{
+                  borderColor: colors.neutral350,
+                  paddingLeft: spacingX._20,
+                  //   backgroundColor: colors.neutral300,
+                }}
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+
+      <View style={styles.footer}>
+        {!loading && (
+          <Button
+            onPress={showLogoutAlert}
+            style={{
+              backgroundColor: colors.rose,
+              height: verticalScale(56),
+              width: verticalScale(56),
+            }}
+          >
+            <Icons.SignOutIcon
+              size={verticalScale(30)}
+              color="white"
+              weight="bold"
+            />
+          </Button>
+        )}
+
+        <Button style={{ flex: 1 }} onPress={onSubmit} loading={loading}>
+          <Typo color={colors.black} fontWeight={"700"}>
+            Update
+          </Typo>
+        </Button>
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+export default ProfileModal;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingHorizontal: spacingY._20,
+    // paddingVertical:spacingY._30,
+  },
+  footer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: spacingX._20,
+    gap: scale(12),
+    paddingTop: spacingY._15,
+    borderTopColor: colors.neutral200,
+    marginBottom: spacingY._10,
+    borderTopWidth: 1,
+  },
+  form: {
+    gap: spacingY._30,
+    marginTop: spacingY._15,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignItems: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+    borderWidth: 1,
+    borderColor: colors.neutral300,
+    // overflow:"hidden",
+    // position:"relative"
+  },
+  edition: {
+    position: "absolute",
+    bottom: spacingY._5,
+    right: spacingY._7,
+    borderRadius: 100,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: spacingY._7,
+  },
+  inputContainer: {
+    gap: spacingY._7,
+  },
+});
